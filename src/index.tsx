0bda4f2cd5f4653b856dc0bcf23b5efe6ad15da3
huyenduc <duc.phamhuyen@ncc.asia>
2023-04-07 00:53:32 +0700

    pushcode

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Login } from './page/login';
+import { Login } from './page/loginPage/login';
 
 
 const root = ReactDOM.createRoot(
